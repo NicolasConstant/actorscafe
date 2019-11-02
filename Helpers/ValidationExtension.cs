aa0813a8fdb96af0d1b0c47a3d51e5b386b4fda7
Xeltica <xeltica@gmail.com>
2019-11-02 20:52:54 +0900

    酷いユーザー名が作れちゃうバグを修正

--- Helpers/ValidationExtension.cs
+++ Helpers/ValidationExtension.cs
@@ -4,6 +4,6 @@ namespace ActorsCafe
 {
     public static class ValidationExtension
     {
-        public static bool IsValidUserName(this string str) => Regex.IsMatch(str, "[A-Za-z_][A-Za-z0-9_]*");
+        public static bool IsValidUserName(this string str) => Regex.IsMatch(str, "^[A-Za-z_][A-Za-z0-9_]*$");
     }
 }
