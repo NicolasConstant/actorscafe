b5f72ccbe0b90ffc2771bf0991c34e989d3f1e29
Xeltica <xeltica@gmail.com>
2019-11-07 12:36:54 +0900

    デザイン適用

--- ClientApp/src/components/ui/UIButton.tsx
+++ ClientApp/src/components/ui/UIButton.tsx
@@ -1,8 +1,8 @@
 import React, { ButtonHTMLAttributes } from "react";
 import css from "./UI.module.scss";
 
-export function UIButton(props: ButtonHTMLAttributes<HTMLButtonElement> & { children?: any, link?: boolean, inline?: boolean }) {
+export function UIButton(props: ButtonHTMLAttributes<HTMLButtonElement> & { children?: any, link?: boolean, inline?: boolean, static?: boolean }) {
     return (
-        <button{...props} className={props.link ? css.link : css.button} style={{ ...props.style, display: props.inline ? "inline-flex" : "flex" }}>{props.children}</button>
+        <button{...props} className={props.link ? css.link : props.static ? css.static : css.button} style={{ ...props.style, display: props.inline ? "inline-flex" : "flex" }}>{props.children}</button>
     );
 }
