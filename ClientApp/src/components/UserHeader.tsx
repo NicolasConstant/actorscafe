fc43dee757809267667b7e634db27c343c9ca347
Xeltica <xeltica@gmail.com>
2019-11-08 21:04:22 +0900

    ユーザーページのデザイン調整

--- ClientApp/src/components/UserHeader.tsx
+++ ClientApp/src/components/UserHeader.tsx
@@ -3,6 +3,7 @@ import { User } from "../models/User";
 import { Link } from "react-router-dom";
 import { useStore } from "../store/module";
 import { toAcctString } from "../helpers/toAcctString";
+import { Card } from "./Card";
 
 export function UserHeader(props: { user: User, onFollowButtonClicked?: (ev: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void, }) {
     const u = props.user;
@@ -10,43 +11,43 @@ export function UserHeader(props: { user: User, onFollowButtonClicked?: (ev: Rea
     const store = useStore();
 
     return (
-        <div>
-            <h1>{u.profileName || u.name}</h1>
-            <p>ID: {acct} { u.isLocked ? "🔒" : "" }</p>
+        <Card>
+            <h1>{u.profileName || u.name}&nbsp;<small>{acct} {u.isLocked ? "🔒" : ""}</small></h1>
+
             <p>
-                { u.isAdmin ? "[Admin]" : "" }
-                { u.isModerator ? "[Mod]" : "" }
-                { u.isBot ? "[BOT]" : "" }
-                { u.isCat ? "[CAT]" : "" }
+                {u.isAdmin ? "[Admin]" : ""}
+                {u.isModerator ? "[Mod]" : ""}
+                {u.isBot ? "[BOT]" : ""}
+                {u.isCat ? "[CAT]" : ""}
             </p>
             {
-                u.isFreezed ? <p>"このユーザーは凍結されています。</p> : 
-                <React.Fragment>
-                    { 
-                        u.isBlocked && u.isBlockingMe ? <p>相互ブロック状態です</p> :
-                        u.isBlocked ? <p>ブロックしています</p> : 
-                        u.isBlockingMe ? <p>ブロックされています</p> : null
-                    }
-                    { u.isFollowingMe ? <p>フォローされています</p> : null }
-                    <p>{u.description || "自己紹介はありません。"}</p>
-                    <p>
-                        <Link to={`/@${u.name}`}>{u.postsCount} 投稿</Link>
-                        &emsp;
+                u.isFreezed ? <p>"このユーザーは凍結されています。</p> :
+                    <React.Fragment>
+                        {
+                            u.isBlocked && u.isBlockingMe ? <p>相互ブロック状態です</p> :
+                                u.isBlocked ? <p>ブロックしています</p> :
+                                    u.isBlockingMe ? <p>ブロックされています</p> : null
+                        }
+                        {u.isFollowingMe ? <p>フォローされています</p> : null}
+                        <p>{u.description || "自己紹介はありません。"}</p>
+                        <p>
+                            <Link to={`/@${u.name}`}>{u.postsCount} 投稿</Link>
+                            &emsp;
                         <Link to={`/@${u.name}/following`}>{u.followingsCount} フォロー</Link>
-                        &emsp;
+                            &emsp;
                         <Link to={`/@${u.name}/followers`}>{u.followersCount} フォロワー</Link>
-                    </p>
-                    { 
-                        store.user && u.id !== store.user.id ? 
-                            <div>
-                                <button onClick={(s) => { if (props.onFollowButtonClicked) { props.onFollowButtonClicked(s); }}}>
-                                    {u.isFollowed ? "フォロー解除" : "フォローする"}
-                                </button>
-                            </div> 
-                        : null 
-                    }
-                </React.Fragment>
+                        </p>
+                        {
+                            store.user && u.id !== store.user.id ?
+                                <div>
+                                    <button onClick={(s) => { if (props.onFollowButtonClicked) { props.onFollowButtonClicked(s); } }}>
+                                        {u.isFollowed ? "フォロー解除" : "フォローする"}
+                                    </button>
+                                </div>
+                                : null
+                        }
+                    </React.Fragment>
             }
-        </div>
+        </Card>
     );
 }
