540ab8ec3bd0f18f0194a9b23816c4100e5018c4
Xeltica <xeltica@gmail.com>
2019-11-09 01:13:37 +0900

    ユーザー情報を更新するように

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { Route } from "react-router";
-import { CSSTransition } from "react-transition-group";
+import "sanitize.css";
 
 import { Layout } from "./pages/Layout";
 import { Index } from "./pages/Index";
@@ -12,8 +12,10 @@ import { Setting } from "./pages/Setting";
 import { UserDirectory } from "./pages/Users/Directory";
 import { ToS } from "./pages/ToS";
 import { Posts } from "./pages/Posts";
-
-import "sanitize.css";
+import { useStore, mod } from "./store/module";
+import { useDispatch } from "react-redux";
+import { postAsync } from "./services/api";
+import { User } from "./models/User";
 
 const route = [
   { path: "/", Component: Index },
@@ -28,6 +30,15 @@ const route = [
 ];
 
 export function App() {
+  const store = useStore();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (store.token) {
+      postAsync<User>("me")
+        .then(u => dispatch(mod.actions.setUser(u)));
+    }
+  }, [store.token]);
+
   return (
     <Layout>{
       route.map(({ path, Component }) => (
